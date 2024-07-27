b14c21f661afe74b3363d9fb59c1156c307668c7
oksana-shpak <oksana.shpak@convertdigital.com.au>
2024-07-27 15:25:48 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const h2 = document.querySelector('h2');
+const count = h2.children.length - 1;
+
+onscroll = () => {
+  const i = (document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * count | 0;
+  console.log(i);
+  const active = h2.querySelector('.active');
+  const next = h2.children[i];
+
+  active?.classList.remove('active');
+  next.classList.add('active');
+}
